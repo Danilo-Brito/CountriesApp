1e068ec5d1b44716e3ed68cfd08ca6103232a46f
Danilo Brito <danilomelo.brito19@gmail.com>
2021-03-03 05:47:13 -0300

    implementing model,viewModel and build the screen layout

--- app/src/main/java/dbrito/com/br/countriesapp/model/CountryModel.java
+++ app/src/main/java/dbrito/com/br/countriesapp/model/CountryModel.java
@@ -1,4 +1,27 @@
 package dbrito.com.br.countriesapp.model;
 
 public class CountryModel {
+    String countryName;
+    String capital;
+    String flag;
+
+    public CountryModel(String countryName, String capital, String flag) {
+        this.countryName = countryName;
+        this.capital = capital;
+        this.flag = flag;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+
 }
--- app/src/main/java/dbrito/com/br/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/dbrito/com/br/countriesapp/viewmodel/ListViewModel.java
@@ -1,4 +1,35 @@
 package dbrito.com.br.countriesapp.viewmodel;
 
-public class ListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dbrito.com.br.countriesapp.model.CountryModel;
+
+public class ListViewModel extends ViewModel {
+    public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<List<CountryModel>>();
+    public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<Boolean>();
+    public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
+    //LiveData is objects observable that generates async values
+
+    public void refresh(){
+        fetchCountries();
+    }
+
+    private void fetchCountries(){
+        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
+        CountryModel country2 = new CountryModel("Brazil", "Brasilia", "");
+        CountryModel country3 = new CountryModel("Czechia", "Praha", "");
+
+        List<CountryModel> list = new ArrayList<>();
+        list.add(country1);
+        list.add(country2);
+        list.add(country3);
+
+        countries.setValue(list);
+        countryLoadError.setValue(false);
+        loading.setValue(false);
+    }
 }
